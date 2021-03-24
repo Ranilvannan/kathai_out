1a74fcbfdd7dfec7e8240de80e9983c809407775
vetrivel <rameshinkumar627@gmail.com>
2021-03-24 14:17:41 +0530

    co

--- story_reader.sh
+++ story_reader.sh
@@ -4,7 +4,7 @@ echo "Mongdb URI"
 export MONGO_URI="mongodb://localhost:27017/story"
 
 echo "Import Path"
-export IMPORT_PATH="/tmp"
+export IMPORT_PATH="/home/vetrivel/story"
 
 echo "Flask"
 export FLASK_APP="application.py"
